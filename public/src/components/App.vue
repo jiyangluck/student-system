480bf6d1a588d5392c59d7b8a95a68b1b9b493cc
lee <ljyteach@163.com>
2017-04-05 11:06:18 +0800

    x

--- public/src/components/App.vue
+++ public/src/components/App.vue
@@ -1,5 +1,42 @@
 <template>
 	<div>
-		<h1>hello vue</h1>
+		<h1>hello vue ajax</h1>
+		<button v-on:click="getData">获取数据</button>
+		<table>
+			<thead>
+				<th>姓名</th>
+				<th>年龄</th>
+				<th>性别</th>
+			</thead>
+			<tbody>
+				<tr v-for="student in students">
+					<td>{{student.name}}</td>
+					<td>{{student.age}}</td>
+					<td>{{student.sex}}</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </template>
+
+<script>
+	import Vue from 'vue';
+	import VueResource from 'vue-resource';
+	Vue.use(VueResource);
+
+	export default {
+		data(){
+			return {
+				students:null
+			}
+		},
+		methods:{
+			getData(){
+				alert('hehe')
+				this.$http.get('./data/students.json').then(function(res){
+					this.students = res.data;
+				})
+			}
+		}
+	}
+</script>
--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.post("/update",function(req,res){
 	})
 })
 
+app.get("/getstudents",function(req,res){
+	datahundle.selectData("data/students.json",function(err,data){
+		res.send(data);
+	})
+})
+
 
 
 
